3b233663c840928d3d1c70988e6b2d97173dcd97
kunalt4 <kunaltolani17@gmail.com>
2020-03-09 12:58:09 +0000

    collecting signatures - covering segments

---
+++ Algorithmic Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -0,0 +1,29 @@
+# Uses python3
+import sys
+from collections import namedtuple
+
+Segment = namedtuple('Segment', 'start end')
+
+def optimal_points(segments):
+    points = []
+    #write your code here
+    # for s in segments:
+    #     points.append(s.start)
+    #     points.append(s.end)
+    segments = sorted(segments, key= lambda s: (s.end, s.start))
+    current = segments[0].end
+    points.append(current)
+    for seg in segments:
+        if current < seg.start or current > seg.end:
+            current = seg.end
+            points.append(current)
+    return points
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n, *data = map(int, input.split())
+    segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    points = optimal_points(segments)
+    print(len(points))
+    print(*points)
